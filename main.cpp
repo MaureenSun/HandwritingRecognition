4f19e4e88a1091306f6aef9715d6b9ecc2f22da2
Maureen Sun <m242sun@uwaterloo.ca>
2023-05-05 19:04:07 -0400

    separate between training and real data sets

--- main.cpp
+++ main.cpp
@@ -1,21 +1,62 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include <opencv2/opencv.hpp>
 
 int main()
 {
-    // Load an image of Aaron Eckhart from the Labeled Faces in the Wild dataset
-    cv::Mat image = cv::imread("C:/Users/Maureen/People-Counting/lfw/Aaron_Eckhart/Aaron_Eckhart_0001.jpg");
+    std::vector<cv::Mat> images;
+    std::vector<std::string> labels;
 
-    // Check if the image was loaded successfully
-    if (!image.data)
+    std::string datasetPath = "C:\\Users\\Maureen\\People-Counting\\Ifw";
+    std::vector<std::string> personDirs = {"person_0", "person_1", "person_2"};
+
+    for (const auto& personDir : personDirs)
     {
-        std::cout << "Failed to load image" << std::endl;
-        return -1;
+        std::string personPath = datasetPath + "/" + personDir;
+        std::vector<std::string> imageFiles;
+        cv::glob(personPath + "/*.jpg", imageFiles, false);
+
+        for (const auto& imageFile : imageFiles)
+        {
+            cv::Mat image = cv::imread(imageFile, cv::IMREAD_GRAYSCALE);
+            std::string label = personDir;
+            images.push_back(image);
+            labels.push_back(label);
+        }
     }
 
-    // Display the image
-    cv::imshow("Image", image);
-    cv::waitKey(0);
+    // Split the data into training and test sets
+    int numImages = 45;
+    int numTrain = static_cast<int>(numImages * 0.8); // 80% for training
+    std::vector<int> indices(numImages);
+    for (int i = 0; i < numImages; ++i)
+    {
+        indices[i] = i;
+    }    std::random_shuffle(indices.begin(), indices.end());
 
+    std::vector<cv::Mat> trainImages;
+    std::vector<std::string> trainLabels;
+    std::vector<cv::Mat> testImages;
+    std::vector<std::string> testLabels;
+
+    for (int i = 0; i < numImages; ++i)
+    {
+        int idx = indices[i];
+        if (i < numTrain)
+        {
+            trainImages.push_back(images[idx]);
+            trainLabels.push_back(labels[idx]);
+        }
+        else
+        {
+            testImages.push_back(images[idx]);
+            testLabels.push_back(labels[idx]);
+        }
+    }
+
+    // Train the face recognition model
+    // ...
+    
     return 0;
 }
