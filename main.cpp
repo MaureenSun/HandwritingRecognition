9ddcd4c94534e731daefae06d865237eb678c5cd
Maureen Sun <m242sun@uwaterloo.ca>
2023-05-05 17:50:56 -0400

    Able to use the dataset

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+int main()
+{
+    // Load an image of Aaron Eckhart from the Labeled Faces in the Wild dataset
+    cv::Mat image = cv::imread("C:/Users/Maureen/People-Counting/lfw/Aaron_Eckhart/Aaron_Eckhart_0001.jpg");
+
+    // Check if the image was loaded successfully
+    if (!image.data)
+    {
+        std::cout << "Failed to load image" << std::endl;
+        return -1;
+    }
+
+    // Display the image
+    cv::imshow("Image", image);
+    cv::waitKey(0);
+
+    return 0;
 }
