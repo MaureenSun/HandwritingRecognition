2012fb58447e9c9f7bc4bdc6cec89b7d36531e10
Maureen Sun <m242sun@uwaterloo.ca>
2023-05-15 23:05:02 -0400

    continuing the CNN architecture

--- main.cpp
+++ main.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm> // for std::shuffle
+#include <random> // for std::default_random_engine
 #include <opencv2/opencv.hpp>
+#include <tensorflow/core/public/session.h>
+#include <tensorflow/cc/ops/standard_ops.h>
+
+using namespace tensorflow;
+using namespace tensorflow::ops;
 
 int main()
 {
     std::vector<cv::Mat> images;
     std::vector<std::string> labels;
 
-    std::string datasetPath = "C:\\Users\\Maureen\\People-Counting\\Ifw";
+    std::string datasetPath = "C:\Users\\Maureen\\People-Counting\\Ifw";
     std::vector<std::string> personDirs = {"person_0", "person_1", "person_2"};
 
     for (const auto& personDir : personDirs)
     {
-        std::string personPath = datasetPath + "/" + personDir;
+        std::string personPath = datasetPath + "\\" + personDir;
         std::vector<std::string> imageFiles;
         cv::glob(personPath + "/*.jpg", imageFiles, false);
 
@@ -26,14 +33,19 @@ int main()
         }
     }
 
-    // Split the data into training and test sets
+    // Split data in traiing and test
     int numImages = 45;
     int numTrain = static_cast<int>(numImages * 0.8); // 80% for training
     std::vector<int> indices(numImages);
     for (int i = 0; i < numImages; ++i)
     {
         indices[i] = i;
-    }    std::random_shuffle(indices.begin(), indices.end());
+    }
+
+    // Shuffling indices....
+    std::random_device rd;
+    std::default_random_engine engine(rd());
+    std::shuffle(indices.begin(), indices.end(), engine);
 
     std::vector<cv::Mat> trainImages;
     std::vector<std::string> trainLabels;
@@ -55,8 +67,37 @@ int main()
         }
     }
 
-    // Train the face recognition model
-    // ...
+
+    // CNN architecture
+    auto input = Placeholder(DT_FLOAT, Placeholder::Shape({None, 784}));
+    auto weights = Variable(RandomNormal(Shape({784, 10})));
+    auto bias = Variable(RandomNormal(Shape({10})));
+    auto output = Softmax(MatMul(input, weights) + bias);
+
+    //  loss function
+    auto labels = Placeholder(DT_FLOAT, Placeholder::Shape({None, 10}));
+    auto cross_entropy = ReduceMean(-ReduceSum(labels * Log(output), {1}));
+
+    auto optimizer = GradientDescentOptimizer(0.5);
+    auto train = optimizer->Minimize(cross_entropy);
+
+    Session* session;
+    TF_CHECK_OK(NewSession(SessionOptions(), &session));
+    TF_CHECK_OK(session->Run({Assign(weights, ...), Assign(bias, ...)}, nullptr));
+    for (int epoch = 0; epoch < num_epochs; epoch++) {
+        for (int batch = 0; batch < num_batches; batch++) {
+            // Next part of data
+            auto x_batch = ...;
+            auto y_batch = ...;
+
+            TF_CHECK_OK(session->Run({{input, x_batch}, {labels, y_batch}}, {}, {train}, nullptr));
+        }
+    }
+
+    auto x_test = ...;
+    auto y_test = ...;
+    auto correct_predictions = ...;
+    
     
     return 0;
 }
